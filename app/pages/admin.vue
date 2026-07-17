55e7a2d3e0cfad4d6f762c8fc72df5f07c6f296c
Michael Wolf <mwolf@pi3g.com>
2026-07-17 13:36:43 +0200

    bulding out admin page

---
+++ app/pages/admin.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <admin_current_user />
+    <div v-for="item in admin_ui[0]!.items" :key="item.name">
+      
+    </div>
+  </v-container>
+</template>
+
+<script setup lang="ts">    
+// import application from '~/metadata/tables/admin_app'
+
+const admin_ui = [
+{
+    name: 'administration',
+    "items":[
+        {name:'current_user', type:'component', permission:'account.set', icon:'mdi-account-switch'},
+        {name:'account', type:'table', permission:'account', icon:'mdi-account'},
+        {name:'role', type:'table', permission:'role', icon:'mdi-role'},
+        {name:'user', type:'table', permission:'user', icon:'mdi-user'}
+    ],
+},
+// application
+]
+
+async function get_admin_ui() {
+}
+
+const { t } = useI18n()
+// const admin_app_meta = admin_app(t);
+</script>
