28492e28bd385f7da146153b7a98655967fbe56c
Michael Wolf <mwolf@pi3g.com>
2026-07-17 14:08:21 +0200

    bulding out admin page

--- app/pages/admin.vue
+++ app/pages/admin.vue
@@ -2,7 +2,8 @@
   <v-container>
     <div v-for="section in admin_ui" :key="section.name">
       <div v-for="item in section.items" :key="item.name">
-        <component v-if="item.type === 'component'" :is="item.component" />
+        <component v-if="item.type === 'component'" :is="item.data" />
+        <Table v-if="item.type === 'table'" :meta="'admin_table'" />
       </div>
     </div>
   </v-container>
@@ -10,16 +11,19 @@
 
 <script setup lang="ts">    
 import admin_current_user from '~/components/admin_current_user.vue'
+import accountTableMetaFcn from '~/schemas/accounts'
+import roleTableMetaFcn from '~/schemas/roles'
 // import application from '~/metadata/tables/admin_app'
+
+const { t } = useI18n();
 
 const admin_ui = [
 {
     name: 'administration',
     "items":[
-        {name:'admin_current_user', component:admin_current_user, type:'component', permission:'account.set', icon:'mdi-account-switch'},
-        {name:'account', type:'table', permission:'account', icon:'mdi-account'},
-        {name:'role', type:'table', permission:'role', icon:'mdi-role'},
-        {name:'user', type:'table', permission:'user', icon:'mdi-user'}
+        {name:'admin_current_user', data:admin_current_user, type:'component', permission:'account.set', icon:'mdi-account-switch'},
+        {name:'account', data:accountTableMetaFcn(t), type:'table', permission:'account', icon:'mdi-account'},
+        {name:'role', data:roleTableMetaFcn(t), type:'table', permission:'role', icon:'mdi-role'},
     ],
 },
 // application
